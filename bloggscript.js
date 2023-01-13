4c0ae06155443ae0b4fff65e03e87f1da28bea55
Siva Dharshini Manickavasagam <118007713+Siva-Dharshini@users.noreply.github.com>
2023-01-13 22:32:28 +0530

    updating for blogg page

--- bloggscript.js
+++ bloggscript.js
@@ -1,1 +1,14 @@
-const readonlyButton= document.getElementById
+const posts =JSON.parse(localStorage.getItem("posts"))
+const postNumber =localStorage.getItem("postNumber")
+const post=posts[Number(postNumber)]
+console.log(post)
+
+const container=document.getElementById("container-post")
+const postHtml=`
+    <h1> ${post.title}</h1>
+    <p>${post.description}</p>
+    <img src="${post.imageLink}">
+    `
+const div=document.createElement("div")
+div.innerHTML=postHtml
+container.appendChild(div)
--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-const createBlogCard = (title, date, description) => {
+const createBlogCard = (title, date, description,idx) => {
   return `
         <div class="card col" style="font-size: 12px; border: none;"> 
             <p class="text-sm" style="color: rgb(128, 128, 128);">${date.toString()}</p>
             <h3 style="font-size: 16px;">${title}</h3>
             <p>${description}</p>
 
-            <a href="./blogg.html"class="text-primary" href="" style="text-decoration: none;">Read More</a>
+            <button index="${idx}" id="read-more" href="./blogg.html" class="text-primary" href="" style="text-decoration: none;">Read More</button>
+
         </div>
     `;
 };
 
+
+
 const posts = JSON.parse(localStorage.getItem("posts"));
 
-posts.forEach((post) => {
-  const postHtml = createBlogCard(post.title, new Date(), post.description);
+posts.forEach((post,i) => {
+  const postHtml = createBlogCard(post.title, new Date(), post.description,i);
   const postsContainer = document.getElementById("blog-tile-container");
   const postDiv = document.createElement("div");
   postDiv.innerHTML = postHtml;
 
   postsContainer.appendChild(postDiv);
 });
+
+const readMorelinks=document.querySelectorAll("#read-more")
+console.log(readMorelinks)
+readMorelinks.forEach((link) =>
+link.onclick=()=>{
+  const index=link.getAttribute("index")
+  localStorage.setItem("postNumber",index)
+
+location.href="./blogg.html"
+})
